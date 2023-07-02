d7d897b6de4a3fda3e2c4cce103a66b252f4734a
HendinTom <HeninTomV.H@Gmail.com>
2023-07-01 20:48:07 -0400

    Got the api to work, but now I need to check if my token is hidden properly

--- weather.py
+++ weather.py
@@ -1,1 +1,9 @@
-print("Hello weather world!!")
+import requests
+from config import TOKEN
+
+latitude = 43.722
+longitude = -79.748
+response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={TOKEN}')
+
+print(response)
+print(response.json())
