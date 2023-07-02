396c7f8b2453c4b771e6c55abc68424920454a3b
HendinTom <HeninTomV.H@Gmail.com>
2023-07-01 23:10:52 -0400

    made the weather api work and implemented a ui

--- weather.py
+++ weather.py
@@ -1,9 +1,34 @@
 import requests
+import json
 from config import TOKEN
 
-latitude = 43.722
-longitude = -79.748
-response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={TOKEN}')
+print("")
+city = input("What city do you need to know the weather in?: ")
+state = input("What is the state code?: ")
+country = input("What is the country code?: ")
 
-print(response)
-print(response.json())
+lonNlat = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&appid={TOKEN}').json()[0]
+
+latitude = lonNlat["lat"]
+longitude = lonNlat["lon"]
+response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={TOKEN}&units=metric')
+data = response.json()["main"]
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+#jprint(response.json())
+
+print("")
+print("================================================================")
+print("")
+print("City: \033[1m" + response.json()["name"] + "\033[0m")
+print("")
+print("Current Temperature:\033[1m", data["temp"], "Celcius\033[0m")
+print("Feels Like:\033[1m", data["feels_like"], "Celcius\033[0m")
+print("")
+print("Maximum:\033[1m", data["temp_max"], "Celcius\033[0m")
+print("Minimum:\033[1m", data["temp_min"], "Celcius\033[0m")
+print("")
